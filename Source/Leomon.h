3688d3b05ae19f84daaad7e90a759311c4162e61
Yuriy Hanysh <ghanysh@gmail.com>
2016-12-11 17:00:27 +0200

    Add project

---
+++ Source/Leomon.h
@@ -0,0 +1,19 @@
+//
+//  Leomon.h
+//  Leomon
+//
+//  Created by Yuriy Hanysh on 12/11/16.
+//  Copyright © 2016 Yuriy Hanysh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Leomon.
+FOUNDATION_EXPORT double LeomonVersionNumber;
+
+//! Project version string for Leomon.
+FOUNDATION_EXPORT const unsigned char LeomonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Leomon/PublicHeader.h>
+
+
