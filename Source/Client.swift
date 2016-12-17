875f045c27695acddb97f7ed48024187e6754c5e
Yuriy Hanysh <ghanysh@gmail.com>
2016-12-17 10:25:27 +0200

    Add API description and base client

---
+++ Source/Client.swift
@@ -0,0 +1,32 @@
+//
+//  Client.swift
+//  Leomon
+//
+//  Created by Yuriy Hanysh on 12/12/16.
+//  Copyright © 2016 Yuriy Hanysh. All rights reserved.
+//
+
+import Foundation
+
+open class Client {
+    var session: URLSession
+    
+    init() {
+        let configuration = URLSessionConfiguration.default
+        session = URLSession(configuration: configuration)
+    }
+    
+    open func request(_ target: API, completion: (Any?) -> Void) {
+        session.dataTask(with: target.requestURL) { data, response, error in
+            guard let data = data else {
+                let error = error
+                completion(nil)
+                return
+            }
+            
+            let json = try? JSONSerialization.jsonObject(with: data)
+            
+            completion(json)
+        }
+    }
+}
